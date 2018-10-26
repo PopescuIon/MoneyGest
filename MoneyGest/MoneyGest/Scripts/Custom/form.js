35b8e15823e4107b4d4df8fa769dc17133eea482
popescu.ion <popescu.ion@DESKTOP-HENRAUT>
2018-10-26 18:48:35 +0300

    login  design

---
+++ MoneyGest/MoneyGest/Scripts/Custom/form.js
@@ -0,0 +1,10 @@
+﻿var moneyGest = moneyGest || {};
+(function (skillet, $, undefined) {
+    skillet.init = function () {
+   
+    }
+
+})(moneyGest.login = moneyGest.login || {}, jQuery)
+$(function () {
+    moneyGest.login.init();
+})
